03ca826a4c45af5066b5c648b9b823ef7bf73d7c
summarek <49320925+summarek@users.noreply.github.com>
2024-06-27 22:09:16 +0200

    silnia

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+int silnia(int n) {
+    if (n <= 1) return 1;
+    else return n * silnia(n - 1);
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
