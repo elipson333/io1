95b8ed60d328328f3e85098e6a3ea712967b87f1
summarek <49320925+summarek@users.noreply.github.com>
2024-06-27 22:14:45 +0200

    silnia wykonanie

--- projekt1.cpp
+++ projekt1.cpp
@@ -17,8 +17,11 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "1. Oblicz silnie" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+        if(wyjscie == 1) cout << "Silnia z " << a << " to " << silnia(a) << endl;
+
     } while(wyjscie != 0);
     return 0;
 }
