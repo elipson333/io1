b18ee51067848f24f78dd051dc59d9928a4cc5c9
summarek <49320925+summarek@users.noreply.github.com>
2024-06-27 22:15:42 +0200

    funkcja czy pierwsza

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= n / 2; ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
